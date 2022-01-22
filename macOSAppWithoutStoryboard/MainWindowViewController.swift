a074fc16dfcd388ccc08e06e071f50beb53542a9
SoundBuddies <s.schaflitzel@googlemail.com>
2022-01-22 14:26:30 +0100

    added sample TextField

--- macOSAppWithoutStoryboard/MainWindowViewController.swift
+++ macOSAppWithoutStoryboard/MainWindowViewController.swift
@@ -24,12 +24,23 @@ class MainWindowViewController: NSViewController {
 
     private lazy var redBox = FlippedView(frame: NSRect(x: 0, y: 0, width: 50, height: 50))
     
+    private lazy var label = NSTextField(labelWithString: "Hello World")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.addSubview(redBox)
         redBox.wantsLayer = true
         redBox.layer?.backgroundColor = NSColor.red.cgColor
+        
+        view.addSubview(label)
+        label.bezelStyle = .roundedBezel
+        label.wantsLayer = true
+        label.layer?.backgroundColor = NSColor.green.cgColor
+        label.frame = NSRect(x: 100, y: 100, width: 100, height: 100)
+//        label.isEditable = false
+//        label.isSelectable = false
+        
         
     }
 
