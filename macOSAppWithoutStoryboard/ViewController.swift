cd2f5a9a49bfae040633515ca7b16ae580d7a2a9
SoundBuddies <s.schaflitzel@googlemail.com>
2022-01-22 10:06:02 +0100

    Initial Commit

---
+++ macOSAppWithoutStoryboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSAppWithoutStoryboard
+//
+//  Created by Stefan Schaflitzel on 22.01.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
